1aaaf13f69eae20f0f436de1d94c2e58aa00336e
Nickprovs <provost.nikolai@gmail.com>
2020-02-05 10:33:45 -0500

    name plate media query progress

--- components/common/standardButton.js
+++ components/common/standardButton.js
@@ -1,9 +1,10 @@
 import standardButton from "../../styles/standardButton.module.css";
 
 export default function StandardButton(props) {
-  const { children, ...rest } = props;
+  const { children, className, ...rest } = props;
+
   return (
-    <button {...rest} className={standardButton.standardButton}>
+    <button {...rest} className={standardButton.standardButton + " " + className}>
       {children}
     </button>
   );
--- components/namePlate.js
+++ components/namePlate.js
@@ -3,11 +3,13 @@ import namePlate from "../styles/nameplate.module.css";
 import StandardButton from "./common/standardButton";
 
 export default function NamePlate(props) {
+  console.log("in name plate", namePlate.namePlateButton);
+
   return (
     <div className={namePlate.namePlateContainer}>
       <Link href="/index">
         <a>
-          <StandardButton style={{ width: "300px", height: "70px" }}>Nicholas Provost</StandardButton>
+          <StandardButton className={namePlate.namePlateButton}>Nicholas Provost</StandardButton>
         </a>
       </Link>
     </div>
