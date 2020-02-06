212032acc9d5d736d996b75578261ac45f21029b
Nickprovs <provost.nikolai@gmail.com>
2020-02-06 13:07:50 -0500

    scroll to top when routing with Router.push

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import "../styles.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-
+import Router from "next/router";
 // This default export is required in a new `pages/_app.js` file.
+
 export default function MyApp({ Component, pageProps }) {
-  console.log("hi");
+  //Routing done with Router.push doesn't scroll to top. This is a fix for that.
+  Router.events.on("routeChangeComplete", () => {
+    window.scrollTo(0, 0);
+  });
+
   return <Component {...pageProps} />;
 }
