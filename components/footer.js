02214e3537ccaf97b7c8b10a9c289c88a98d8659
Nickprovs <provost.nikolai@gmail.com>
2020-02-05 23:10:52 -0500

    Copyright info

--- components/footer.js
+++ components/footer.js
@@ -17,16 +17,22 @@ export default function Footer(props) {
   };
 
   return (
-    <div className={footer.footer}>
-      <ul style={externalPageListStyle}>
-        {externalPages.map(item => (
-          <li style={externalPageListItemStyle} key={item.label}>
-            <a href={item.href}>
-              <i style={externalPageIconStyle} className={item.iconClasses}></i>
-            </a>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <div className={footer.footer}>
+        <ul style={externalPageListStyle}>
+          {externalPages.map(item => (
+            <li style={externalPageListItemStyle} key={item.label}>
+              <a href={item.href}>
+                <i style={externalPageIconStyle} className={item.iconClasses}></i>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className={footer.copyrightContainer}>
+        <label className={footer.copyright}>© NICHOLAS PROVOST | BUILT BY NICHOLAS PROVOST | INSPIRED BY ANDRE BENZ</label>
+      </div>
     </div>
   );
 }
