2075dbdeaa256b31d10df59dcb2ae58f86a6d265
Nickprovs <provost.nikolai@gmail.com>
2020-02-12 11:07:36 -0500

    playing with the idea of a photoservice. Fake async data passed to client for toying.

--- pages/showcase/photo.js
+++ pages/showcase/photo.js
@@ -1,26 +1,25 @@
 import Layout from "../../components/layout";
+import { getPhotosAsync } from "../../services/photoService";
 
-export default function Photo() {
+export default function Photo(props) {
+  const { photos } = props;
+  console.log("photos!", photos);
   return (
     <Layout>
       <h1 className="mainContentTitle">Photo</h1>
-
-      <div style={{ width: "50vw", height: "50vh" }} style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <a
-          style={{ width: "50vw", height: "50vh" }}
-          data-flickr-embed="true"
-          data-context="true"
-          href="https://www.flickr.com/photos/186871940@N02/49513802168/in/album-72157713047888003/"
-          title="wt2zylestrkprrd2l6cxolh7geo12t0o"
-        >
-          <img
-            src="https://live.staticflickr.com/65535/49513802168_90131fc81e_c.jpg"
-            style={{ width: "50vw", height: "50vh" }}
-            alt="wt2zylestrkprrd2l6cxolh7geo12t0o"
-          />
-        </a>
-        <script async src="//embedr.flickr.com/assets/client-code.js" charSet="utf-8"></script>
+      <div>
+        {photos.map(photo => (
+          <label>{photo.src}</label>
+        ))}
       </div>
     </Layout>
   );
 }
+
+Photo.getInitialProps = async function() {
+  const res = await getPhotosAsync();
+  console.log("Got Data", res);
+  return {
+    photos: res.photos
+  };
+};
---
+++ services/photoService.js
@@ -0,0 +1,82 @@
+import asyncUtilities from "../util/asyncUtilities";
+
+//Note: Use Isomorphic-Unfetch when actually making rest calls
+//Unlike other http clients, it'll work on server and client
+
+export async function getPhotosAsync() {
+  await asyncUtilities.setTimeoutAsync(200);
+  return {
+    photos: [
+      {
+        orientation: "landscape",
+        size: "large",
+        src: "https://cdna.artstation.com/p/assets/images/images/023/453/030/large/andreas-rocha-wizardstower01.jpg"
+      },
+      {
+        orientation: "portrait",
+        size: "large",
+        src: "https://cdnb.artstation.com/p/assets/images/images/023/551/031/large/jiwoo-lee-jiwoo-padawan-fanart.jpg"
+      },
+      {
+        orientation: "portrait",
+        size: "large",
+        src: "https://cdnb.artstation.com/p/assets/images/images/023/723/551/small/ishmael-hoover-eaglewarrior3.jpg"
+      },
+      {
+        orientation: "landscape",
+        size: "medium",
+        src: "https://cdna.artstation.com/p/assets/images/images/002/912/664/small/andreas-rocha-thegathering.jpg"
+      },
+      {
+        orientation: "portrait",
+        size: "medium",
+        src: "https://cdnb.artstation.com/p/assets/images/images/000/403/027/small/andreas-rocha-ill-tidings02.jpg"
+      },
+      {
+        orientation: "landscape",
+        size: "medium",
+        src: "https://cdna.artstation.com/p/assets/images/images/000/085/508/small/Alm-Atias-Refuge.jpg"
+      },
+      {
+        orientation: "landscape",
+        size: "medium",
+        src: "https://https://cdnb.artstation.com/p/assets/images/images/000/320/963/small/andreas-rocha-dangerousroads.jpg"
+      },
+      {
+        orientation: "portrait",
+        size: "medium",
+        src: "https://cdnb.artstation.com/p/assets/images/images/001/058/155/large/mike-hill-002-front-view3-copy.jpg"
+      },
+      {
+        orientation: "portrait",
+        size: "medium",
+        src: "https://https://cdnb.artstation.com/p/assets/images/images/000/286/213/small/sparth-firststrike-final.jpg"
+      },
+      {
+        orientation: "landscape",
+        size: "medium",
+        src: "https://cdnb.artstation.com/p/assets/images/images/000/244/079/large/aaron-limonick-lake-vista-key-2-logo.jpg"
+      },
+      {
+        orientation: "portrait",
+        size: "medium",
+        src: "https://cdnb.artstation.com/p/assets/images/images/000/201/563/small/yuri-shwedoff-ellie-the-last-of-us.jpg"
+      },
+      {
+        orientation: "portrait",
+        size: "medium",
+        src: "https://cdna.artstation.com/p/assets/images/images/018/461/496/small/moero-knight-0005-magical-girls.jpg"
+      },
+      {
+        orientation: "landscape",
+        size: "medium",
+        src: "https://cdna.artstation.com/p/assets/images/images/019/964/822/large/kyle-grech-spike.jpg"
+      },
+      {
+        orientation: "landscape",
+        size: "large",
+        src: "https://cdna.artstation.com/p/assets/images/images/017/735/130/large/ege-ozteke-champaloo.jpg"
+      }
+    ]
+  };
+}
