6877c5e59f03f0918f8e8571c99bccaeb1442bd2
Nickprovs <provost.nikolai@gmail.com>
2020-02-12 14:32:59 -0500

    initial software grid toying done.

--- pages/showcase/software.js
+++ pages/showcase/software.js
@@ -1,9 +1,36 @@
 import Layout from "../../components/layout";
+import { getSoftwareProjectPreviewsAsync } from "../../services/softwareService";
+import softwareStyles from "../../styles/software.module.css";
 
-export default function Software() {
+export default function Software(props) {
+  const { previews } = props;
+  console.log("client previews", previews);
   return (
     <Layout>
       <h1 className="mainContentTitle">Software</h1>
+      <div className={softwareStyles.container}>
+        {previews.map(preview => (
+          <div className={softwareStyles.item}>
+            <div className={softwareStyles.previewLabel}>
+              <label>{preview.title}</label>
+            </div>
+            <div className={softwareStyles.previewImage}>
+              <img className={softwareStyles.containerFitImage} src={preview.previewImageSrc} />
+            </div>
+            <div className={softwareStyles.previewText}>
+              <label>{preview.previewText}</label>
+            </div>
+          </div>
+        ))}
+      </div>
     </Layout>
   );
 }
+
+Software.getInitialProps = async function() {
+  const res = await getSoftwareProjectPreviewsAsync();
+  console.log("Got Data", res);
+  return {
+    previews: res.previews
+  };
+};
