8006bbfd1e2e35bd6dc2755bb8a3639e9059df17
Nickprovs <provost.nikolai@gmail.com>
2020-02-16 20:56:59 -0500

    Added config package

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const config = require("config");
 const app = express();
 
 require("./startup/routes")(app);
+require("./startup/config")();
 
-const port = 8080;
+const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 module.exports = server;
