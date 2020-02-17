e9ca7345cd7a6a20da17615eb38d0a74cc7723d4
Nickprovs <provost.nikolai@gmail.com>
2020-02-16 22:07:47 -0500

    Added helmet and compression packages and applied to express.

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
