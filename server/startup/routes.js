3d0aadc7aa32fa20651a65d31ad8d84495761cb9
Nickprovs <provost.nikolai@gmail.com>
2020-02-16 20:45:49 -0500

    added main routes for my website

--- server/startup/routes.js
+++ server/startup/routes.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const blogs = require("../routes/blogs");
+const software = require("../routes/software");
+const photos = require("../routes/photos");
+const videos = require("../routes/videos");
 
 module.exports = function(app) {
   app.use("/blogs", blogs);
+  app.use("/software", software);
+  app.use("/photos", photos);
+  app.use("/videos", videos);
 };
