d7d6d0eb2ffec148b74933f91f1c745148a18ffa
Nickprovs <provost.nikolai@gmail.com>
2020-02-17 20:59:47 -0500

    Added some more tests for the blogs route

--- server/routes/blogs.js
+++ server/routes/blogs.js
@@ -2,9 +2,20 @@ const express = require("express");
 const moment = require("moment");
 const { Blog, validate } = require("../models/blog");
 const router = express.Router();
+const validateObjectId = require("../middleware/validateObjectId");
 
-router.get("/", (req, res) => {
-  res.send("hello!");
+router.get("/", async (req, res) => {
+  const blogs = await Blog.find()
+    .select("-__v")
+    .sort("title");
+  res.send(blogs);
+});
+
+router.get("/:id", validateObjectId, async (req, res) => {
+  const blog = await Blog.findById(req.params.id).select("-__v");
+  if (!blog) return res.status(404).send("The blog with the given ID was not found.");
+
+  res.send(blog);
 });
 
 router.post("/", async (req, res) => {
--- server/tests/integration/blogs.test.js
+++ server/tests/integration/blogs.test.js
@@ -3,39 +3,100 @@ const { Blog } = require("../../models/blog");
 const mongoose = require("mongoose");
 const moment = require("moment");
 
+let server;
 describe("/blogs", () => {
-  let server;
-  let blog;
-
-  const exec = () => {
-    return request(server)
-      .post("/blogs")
-      .send({ blog });
-  };
-
-  beforeEach(async () => {
+  beforeEach(() => {
     server = require("../../index");
-
-    blogId = mongoose.Types.ObjectId();
-
-    blog = new Blog({
-      title: "testtt",
-      datePosted: moment().toJSON(),
-      dateLastModified: moment().toJSON(),
-      previewText: "The dogiest of dogs.",
-      previewImageSource: "https://i.imgur.com/O2NQNvP.jpg",
-      body: "aadada"
-    });
-    await blog.save();
   });
-
   afterEach(async () => {
     await server.close();
-    await Blog.remove({});
+    await Blog.deleteMany({});
   });
 
-  it("should return 200 if successful", async () => {
-    const res = await exec();
-    expect(res.status).toBe(200);
+  describe("GET /", () => {
+    it("Should return all the blogs", async () => {
+      const blogs = [
+        {
+          title: "dogs",
+          datePosted: moment().toJSON(),
+          dateLastModified: moment().toJSON(),
+          previewText: "The dogiest of dogs.",
+          previewImageSource: "https://i.imgur.com/O2NQNvP.jpg",
+          body: "aadada"
+        },
+        {
+          title: "cats",
+          datePosted: moment().toJSON(),
+          dateLastModified: moment().toJSON(),
+          previewText: "The catiest of cats.",
+          previewImageSource: "https://i.imgur.com/O2NQNvP.jpg",
+          body: "sfsfsfsfsf"
+        }
+      ];
+
+      await Blog.collection.insertMany(blogs);
+      const res = await request(server).get("/blogs");
+      expect(res.status).toBe(200);
+      expect(res.body.length).toBe(2);
+      expect(res.body.some(g => g.title === "dogs")).toBeTruthy();
+      expect(res.body.some(g => g.title === "cats")).toBeTruthy();
+    });
+  });
+
+  describe("GET /:id", () => {
+    it("should return a blog if valid id is passed", async () => {
+      const blog = new Blog({
+        title: "dogs",
+        datePosted: moment().toJSON(),
+        dateLastModified: moment().toJSON(),
+        previewText: "The dogiest of dogs.",
+        previewImageSource: "https://i.imgur.com/O2NQNvP.jpg",
+        body: "aadada"
+      });
+      await blog.save();
+
+      const res = await request(server).get("/blogs/" + blog._id);
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("title", blog.title);
+    });
+
+    it("should return 404 if invalid id is passed", async () => {
+      const res = await request(server).get("/blogs/1");
+      expect(res.status).toBe(404);
+    });
+
+    it("should return 404 if no blog with the given id exists", async () => {
+      const id = mongoose.Types.ObjectId();
+      const res = await request(server).get("/blogs/" + id);
+
+      expect(res.status).toBe(404);
+    });
+  });
+
+  describe("POST /", () => {
+    let blog;
+
+    const exec = () => {
+      return request(server)
+        .post("/blogs")
+        .send({ blog });
+    };
+
+    beforeEach(async () => {
+      blogId = mongoose.Types.ObjectId();
+      blog = new Blog({
+        title: "testtt",
+        datePosted: moment().toJSON(),
+        dateLastModified: moment().toJSON(),
+        previewText: "The dogiest of dogs.",
+        previewImageSource: "https://i.imgur.com/O2NQNvP.jpg",
+        body: "aadada"
+      });
+    });
+
+    it("should return 200 if successful", async () => {
+      const res = await exec();
+      expect(res.status).toBe(200);
+    });
   });
 });
