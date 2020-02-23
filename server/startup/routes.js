23084c8553912fa9c7d85956468d107720f10787
Nickprovs <provost.nikolai@gmail.com>
2020-02-23 17:05:05 -0500

    added article, photo, and video category routes. May change routing strucutre. But I'll start with this simple setup for now.

--- server/startup/routes.js
+++ server/startup/routes.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const auth = require("../routes/auth");
+const articleCategories = require("../routes/articleCategories");
 const articles = require("../routes/articles");
+const photoCategories = require("../routes/photoCategories");
 const photos = require("../routes/photos");
+const videoCategories = require("../routes/videoCategories");
 const videos = require("../routes/videos");
 
 module.exports = function(app) {
   app.use("/auth", auth);
+
   app.use("/articles", articles);
+  app.use("/articleCategories", articleCategories);
+
   app.use("/photos", photos);
+  app.use("/photoCategories", photoCategories);
+
   app.use("/videos", videos);
+  app.use("/videoCategories", videoCategories);
 };
