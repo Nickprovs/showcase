5c9883f03773fb556f9f2c9d5f8088ebc082cf9b
Nickprovs <provost.nikolai@gmail.com>
2020-02-25 12:48:42 -0500

    Categories is now a reusable route that takes in a category model and joi validation schema.

---
+++ server/routes/common/categories.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const auth = require("../../middleware/auth");
+const admin = require("../../middleware/admin");
+const validateBody = require("../../middleware/validateBody");
+const validateObjectId = require("../../middleware/validateObjectId");
+const winston = require("winston");
+
+module.exports = function(CategoryModel, categoryJoiSchema) {
+  const router = express.Router();
+
+  router.get("/", async (req, res) => {
+    const categories = await CategoryModel.find()
+      .select("-__v")
+      .sort({ name: "asc" });
+
+    const total = await CategoryModel.count({});
+    const data = {
+      total: total,
+      items: categories
+    };
+
+    res.send(data);
+  });
+
+  router.get("/:id", validateObjectId, async (req, res) => {
+    const category = await CategoryModel.findById(req.params.id).select("-__v");
+    if (!category) return res.status(404).send("The article category with the given ID was not found.");
+
+    res.send(category);
+  });
+
+  router.post("/", [auth, admin, validateBody(categoryJoiSchema)], async (req, res) => {
+    let category = new CategoryModel({
+      name: req.body.name
+    });
+
+    category = await category.save();
+
+    res.send(category);
+  });
+
+  router.put("/:id", [auth, admin, validateObjectId, validateBody(categoryJoiSchema)], async (req, res) => {
+    const updatedCategoryModel = await CategoryModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.name
+      },
+      { new: true }
+    );
+
+    if (!updatedCategoryModel) return res.status(404).send("Article Category not found.");
+
+    res.send(updatedCategoryModel);
+  });
+
+  router.delete("/:id", [auth, admin, validateObjectId], async (req, res) => {
+    const category = await CategoryModel.findByIdAndRemove(req.params.id);
+    if (!category) return res.status(404).send("The article category with the given ID was not found.");
+
+    res.send(category);
+  });
+
+  return router;
+};
--- server/startup/routes.js
+++ server/startup/routes.js
@@ -1,21 +1,26 @@
-const express = require("express");
 const auth = require("../routes/auth");
-const articleCategories = require("../routes/articleCategories");
 const articles = require("../routes/articles");
-const photoCategories = require("../routes/photoCategories");
+const software = require("../routes/software");
 const photos = require("../routes/photos");
-const videoCategories = require("../routes/videoCategories");
 const videos = require("../routes/videos");
+const categories = require("../routes/common/categories");
+const { ArticleCategory, joiSchema: joiArticleCategorySchema } = require("../models/articleCategory");
+const { SoftwareCategory, joiSchema: joiSoftwareCategorySchema } = require("../models/softwareCategory");
+const { PhotoCategory, joiSchema: joiPhotoCategorySchema } = require("../models/photoCategory");
+const { VideoCategory, joiSchema: joiVideoCategorySchema } = require("../models/videoCategory");
 
 module.exports = function(app) {
   app.use("/auth", auth);
 
   app.use("/articles", articles);
-  app.use("/articleCategories", articleCategories);
+  app.use("/articleCategories", categories(ArticleCategory, joiArticleCategorySchema));
+
+  app.use("/software", software);
+  app.use("/softwareCategories", categories(SoftwareCategory, joiSoftwareCategorySchema));
 
   app.use("/photos", photos);
-  app.use("/photoCategories", photoCategories);
+  app.use("/photoCategories", categories(PhotoCategory, joiPhotoCategorySchema));
 
   app.use("/videos", videos);
-  app.use("/videoCategories", videoCategories);
+  app.use("/videoCategories", categories(VideoCategory, joiVideoCategorySchema));
 };
