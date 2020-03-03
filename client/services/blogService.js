22b622b04ff26f02486cec6ecd879b0a83b3ee1c
Nickprovs <provost.nikolai@gmail.com>
2020-03-03 18:26:03 -0500

    removed unecessary console logs

--- client/services/blogService.js
+++ client/services/blogService.js
@@ -5,8 +5,6 @@ import { APIURL } from "../util/constants";
 const BLOGSAPIURL = `${APIURL}/articles`;
 
 export async function getBlogPreviewsAsync(options) {
-  console.log("Requesting from: ", BLOGSAPIURL);
-
   let query = "";
   if (options.limit) query += query ? `&limit=${options.limit}` : `?limit=${options.limit}`;
   if (options.offset) query += query ? `&offset=${options.offset}` : `?offset=${options.offset}`;
@@ -14,15 +12,12 @@ export async function getBlogPreviewsAsync(options) {
 
   const res = await fetch(BLOGSAPIURL + query);
   const data = await res.json();
-  console.log(data);
   return data;
 }
 
 export async function getBlogAsync(blogIdOrSlug) {
   const requestUrl = BLOGSAPIURL + "/" + blogIdOrSlug;
-  console.log("Requesting from: ", requestUrl);
   const res = await fetch(requestUrl);
   const data = await res.json();
-  console.log(data);
   return data;
 }
