3f766c2d3bed9eface0f34e5426d56246486a07d
Nickprovs <provost.nikolai@gmail.com>
2020-03-03 18:26:26 -0500

    added rudimentary search to blog page. still under dev.

--- client/pages/blog.js
+++ client/pages/blog.js
@@ -4,6 +4,8 @@ import blogStyles from "../styles/blog.module.css";
 import Input from "../components/common/input";
 import Pagination from "../components/common/pagination";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 const pageSize = 6;
 
@@ -11,11 +13,35 @@ export default function Blog(props) {
   const { previews, currentPage, totalBlogsCount } = props;
 
   console.log("current page via client", currentPage);
+  const [searchText, setSearchText] = useState("");
+  const router = useRouter();
+
+  const search = e => {
+    e.preventDefault();
+    console.log(searchText);
+    router.push({
+      pathname: router.pathname,
+      query: { search: searchText }
+    });
+  };
+
+  const handleTextChange = e => {
+    setSearchText(e.target.value);
+    router.push({
+      pathname: router.pathname,
+      query: { search: e.target.value }
+    });
+  };
 
   return (
     <Layout>
-      <form onSubmit={() => console.log("submitted")}>
-        <Input placeholder="Search..." style={{ width: "30%", marginLeft: "20px" }} />
+      <form onSubmit={e => search(e)}>
+        <Input
+          onChange={e => handleTextChange(e)}
+          value={searchText}
+          placeholder="Search..."
+          style={{ width: "30%", marginLeft: "20px" }}
+        />
       </form>
       <div className={blogStyles.container}>
         {previews.map(preview => (
@@ -47,11 +73,15 @@ export default function Blog(props) {
 
 Blog.getInitialProps = async function(context) {
   let currentPage = 1;
+  let search = "";
+
   if (context.query.page) currentPage = parseInt(context.query.page);
+  if (context.query.search) search = context.query.search;
 
   const getOptions = {
     offset: (currentPage - 1) * pageSize,
-    limit: pageSize
+    limit: pageSize,
+    search: search
   };
 
   const res = await getBlogPreviewsAsync(getOptions);
