0ee809570c9b14dc669e374b25882f7dd93b9243
Nickprovs <provost.nikolai@gmail.com>
2020-03-05 13:24:52 -0500

    added select for future forms

---
+++ client/components/common/select.js
@@ -0,0 +1,12 @@
+import select from "../../styles/select.module.css";
+
+export default function Select(props) {
+  const { className, text, isSelected, ...rest } = props;
+
+  if (isSelected) mainButtonClasses += select.dropbtnSelected + " ";
+  return (
+    <select {...rest} className={select.select}>
+      {props.children}
+    </select>
+  );
+}
--- client/pages/playground.js
+++ client/pages/playground.js
@@ -1,9 +1,19 @@
 import Layout from "../components/layout";
+import Select from "../components/common/select";
 
 export default function Playground() {
   return (
     <Layout>
-      <h1>Playground</h1>
+      <div style={{ height: "500px" }}>
+        <div style={{ padding: "5px" }}>
+          <Select text="Hey">
+            <option>"Yo"</option>
+            <option>"What's"</option>
+            <option>"Good"</option>
+            <option>"Homie"</option>
+          </Select>
+        </div>
+      </div>
     </Layout>
   );
 }
