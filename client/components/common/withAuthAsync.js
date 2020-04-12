757ac504c5327b52e64cc5359d47b4a8658011c4
Nick Provost <nickprovs@hotmail.com>
2020-04-12 17:01:30 -0400

    removed unecessary duplicate async call in auth component and formatted with prettier

--- client/components/common/withAuthAsync.js
+++ client/components/common/withAuthAsync.js
@@ -1,73 +1,58 @@
 import { getCurrentUserAsync } from "../../services/userService";
-import RedirectUtilities from "../../util/redirectUtilities"
-import {Component} from "react";
-
-
-
+import RedirectUtilities from "../../util/redirectUtilities";
+import { Component } from "react";
 
 const withAuthAsync = (WrappedComponent, redirect = false, redirectUri = "/login") => {
-  return class AuthComponent extends Component{
-
-    static async getInitialProps(ctx){
-
+  return class AuthComponent extends Component {
+    static async getInitialProps(ctx) {
       //Authenticate w/ server
-      const res = await getCurrentUserAsync(ctx);
-      
       let authIssue = true;
       let user = null;
-      try {         
-          const res = await getCurrentUserAsync(ctx);
-          if(res.status === 200){
-            user = await res.json();
-            authIssue = false;
-          }
-      }
-      catch(ex) {
+      try {
+        const res = await getCurrentUserAsync(ctx);
+        if (res.status === 200) {
+          user = await res.json();
+          authIssue = false;
+        }
+      } catch (ex) {
         authIssue = true;
-      }    
-
-      const innerProps =
-        WrappedComponent.getInitialProps &&
-        (await WrappedComponent.getInitialProps(ctx))
-      
-      if(authIssue){
-        if(redirect)
-          RedirectUtilities.Redirect(ctx, redirectUri);
-        return { ...innerProps, user }
       }
 
-      else{
-        return { ...innerProps, user }
+      const innerProps = WrappedComponent.getInitialProps && (await WrappedComponent.getInitialProps(ctx));
+
+      if (authIssue) {
+        if (redirect) RedirectUtilities.Redirect(ctx, redirectUri);
+        return { ...innerProps, user };
+      } else {
+        return { ...innerProps, user };
       }
     }
 
-    constructor(props){
+    constructor(props) {
       super(props);
       this.syncLogout = this.syncLogout.bind(this);
     }
 
     syncLogout(event) {
-      if (event.key === 'logout') {
-        console.log('logged out from storage!')
-        Router.push('/login')
+      if (event.key === "logout") {
+        console.log("logged out from storage!");
+        Router.push("/login");
       }
     }
 
-    componentDidMount(){
-      window.addEventListener('storage', this.syncLogout)
-
+    componentDidMount() {
+      window.addEventListener("storage", this.syncLogout);
     }
 
-    componentWillUnmount(){
-      window.removeEventListener('storage', this.syncLogout)
-      window.localStorage.removeItem('logout')
+    componentWillUnmount() {
+      window.removeEventListener("storage", this.syncLogout);
+      window.localStorage.removeItem("logout");
     }
 
-    render(){
-      return <WrappedComponent {...this.props}/>
+    render() {
+      return <WrappedComponent {...this.props} />;
     }
-
-  }
-}
+  };
+};
 
 export default withAuthAsync;
