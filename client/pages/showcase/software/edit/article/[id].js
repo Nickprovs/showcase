22645c7ee9e80131247ed884528709be0d577565
Nick Provost <nickprovs@hotmail.com>
2020-04-18 17:41:27 -0400

    editing software / categories

---
+++ client/pages/showcase/software/edit/article/[id].js
@@ -0,0 +1,162 @@
+import Layout from "../../../../../components/layout";
+import withAuthAsync from "../../../../../components/common/withAuthAsync";
+import Form from "../../../../../components/common/form";
+import CustomJoi from "../../../../../misc/customJoi";
+import { getSoftwareAsync, getSoftwareCategoriesAsync, updateSoftwareAsync } from "../../../../../services/softwareService";
+import Head from "next/head";
+import { toast, cssTransition } from "react-toastify";
+import Router from "next/router";
+import RouterUtilities from "../../../../../util/routerUtilities";
+import StringUtilities from "../../../../../util/stringUtilities";
+
+class Article extends Form {
+  static async getInitialProps(context) {
+    const { id } = context.query;
+
+    //Get the software
+    let software = null;
+    try {
+      const softwareRes = await getSoftwareAsync(id);
+      software = await softwareRes.json();
+    } catch (ex) {
+      software = null;
+    }
+
+    //Get categories for form
+    let categories = null;
+    try {
+      let categoriesRes = await getSoftwareCategoriesAsync();
+      categories = await categoriesRes.json();
+    } catch (ex) {
+      categories = null;
+    }
+
+    return { software: software, categories: categories };
+  }
+
+  constructor() {
+    super();
+
+    this.state.data = { title: "", slug: "", category: "", image: "", description: "", body: "", tags: "" };
+    this.state.errors = {};
+  }
+
+  async componentDidMount() {
+    const { software, categories } = this.props;
+    if (!software) {
+      toast.error("Couldn't get software. Redirecting back.", { autoClose: 1500 });
+      await RouterUtilities.routeInternalWithDelayAsync("/showcase/software", 2000);
+      return;
+    }
+
+    if (!categories) {
+      toast.error("Couldn't get categories. Redirecting back.", { autoClose: 1500 });
+      await RouterUtilities.routeInternalWithDelayAsync("/showcase/software", 2000);
+      return;
+    }
+
+    this.getStateDataFromSoftware(software);
+  }
+
+  getStateDataFromSoftware(software) {
+    this.setState({
+      data: {
+        title: software.title,
+        slug: software.slug,
+        category: software.category.name,
+        image: software.image,
+        description: software.description,
+        body: software.body,
+        tags: StringUtilities.getCsvStringFromArray(software.tags),
+      },
+    });
+  }
+
+  schema = CustomJoi.object({
+    title: CustomJoi.string().min(2).max(64).required(),
+    slug: CustomJoi.string()
+      .min(2)
+      .max(128)
+      .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/)
+      .required(),
+    category: CustomJoi.string().min(1).required().label("Category"),
+    description: CustomJoi.string().min(1).required().label("Description"),
+    image: CustomJoi.string().min(2).max(1000).required(),
+    body: CustomJoi.string().min(10).required(),
+    tags: CustomJoi.csvString().required().min(3).max(10),
+  });
+
+  getSoftwareFromPassingState() {
+    const { categories } = this.props;
+    let software = { ...this.state.data };
+
+    let category = software.category;
+    delete software.category;
+    software.categoryId = categories.items.filter((c) => c.name == category)[0]._id;
+
+    let tagsString = software.tags;
+    delete software.tags;
+    let tagsArray = tagsString.replace(/^,+|,+$/gm, "").split(",");
+    tagsArray = tagsArray.map((str) => str.trim());
+    software.tags = tagsArray;
+
+    return software;
+  }
+
+  doSubmit = async () => {
+    let originalSoftware = this.props.software;
+    let software = this.getSoftwareFromPassingState();
+    software._id = originalSoftware._id;
+
+    let res = null;
+    //Try and post the new category
+    try {
+      res = await updateSoftwareAsync(software);
+    } catch (ex) {
+      let errorMessage = `Error: ${ex}`;
+      console.log(errorMessage);
+      toast.error(errorMessage);
+      return;
+    }
+    if (!res.ok) {
+      let body = "";
+      //TODO: Parse Text OR JSON
+      body = await res.text();
+      let errorMessage = `Error: ${res.status} - ${body}`;
+      console.log(errorMessage);
+      toast.error(errorMessage);
+      return;
+    }
+
+    //TODO: Disallow posting duplicate category at server level.
+    Router.push("/showcase/software");
+  };
+
+  render() {
+    let { categories, user } = this.props;
+    categories = categories ? categories : [];
+    return (
+      <div>
+        <Head>
+          <script key="tinyMCE" type="text/javascript" src="/static/scripts/tinymce/tinymce.min.js"></script>
+        </Head>
+        <Layout user={user}>
+          <div className="standardPadding">
+            <form onSubmit={this.handleSubmit}>
+              {this.renderTextInput("title", "TITLE")}
+              {this.renderTextInput("slug", "SLUG")}
+              {this.renderSelect("category", "CATEGORY", "", categories.items, "name")}
+              {this.renderTextInput("image", "IMAGE")}
+              {this.renderTextArea("description", "DESCRIPTION")}
+              {this.renderHtmlEditor("body", "BODY")}
+              {this.renderTextInput("tags", "TAGS")}
+              {this.renderButton("UPDATE")}
+            </form>
+          </div>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default withAuthAsync(Article, true);
---
+++ client/pages/showcase/software/edit/category/[id].js
@@ -0,0 +1,109 @@
+import Layout from "../../../../../components/layout";
+import withAuthAsync from "../../../../../components/common/withAuthAsync";
+import Form from "../../../../../components/common/form";
+import CustomJoi from "../../../../../misc/customJoi";
+import { getSoftwareCategoryAsync, updateSoftwareCategoryAsync } from "../../../../../services/softwareService";
+import { toast, cssTransition } from "react-toastify";
+import Router from "next/router";
+import RouterUtilities from "../../../../../util/routerUtilities";
+
+class Category extends Form {
+  static async getInitialProps(context) {
+    const { id } = context.query;
+
+    //Get the category
+    let category = null;
+    try {
+      let categoryRes = await getSoftwareCategoryAsync(id);
+      category = await categoryRes.json();
+    } catch (ex) {
+      category = null;
+    }
+
+    return { category };
+  }
+
+  constructor() {
+    super();
+
+    this.state.data = { name: "", slug: "" };
+    this.state.errors = {};
+  }
+
+  async componentDidMount() {
+    const { category } = this.props;
+    if (!category) {
+      toast.error("Couldn't get category. Redirecting back.", { autoClose: 1500 });
+      await RouterUtilities.routeInternalWithDelayAsync("/showcase/software", 2000);
+      return;
+    }
+
+    this.getStateDataFromCategory(category);
+  }
+
+  getStateDataFromCategory(category) {
+    this.setState({
+      data: {
+        name: category.name,
+        slug: category.slug,
+      },
+    });
+  }
+
+  schema = CustomJoi.object({
+    name: CustomJoi.string().min(2).max(50).required(),
+    slug: CustomJoi.string()
+      .min(2)
+      .max(128)
+      .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/)
+      .required(),
+  });
+
+  doSubmit = async () => {
+    let originalCategory = this.props.category;
+    let category = { ...this.state.data };
+    category._id = originalCategory._id;
+
+    let res = null;
+    //Try and post the new category
+    try {
+      res = await updateSoftwareCategoryAsync(category);
+    } catch (ex) {
+      let errorMessage = `Error: ${ex}`;
+      console.log(errorMessage);
+      toast.error(errorMessage);
+      return;
+    }
+    if (!res.ok) {
+      let body = "";
+      //TODO: Parse Text OR JSON
+      body = await res.text();
+      let errorMessage = `Error: ${res.status} - ${body}`;
+      console.log(errorMessage);
+      toast.error(errorMessage);
+      return;
+    }
+
+    //TODO: Disallow posting duplicate category at server level.
+    Router.push("/showcase/software");
+  };
+
+  render() {
+    let { user } = this.props;
+    return (
+      <div>
+        <Layout user={user}>
+          <div className="standardPadding">
+            <form onSubmit={this.handleSubmit}>
+              {this.renderTextInput("name", "NAME")}
+              {this.renderTextInput("slug", "SLUG")}
+              {this.renderButton("UPDATE")}
+            </form>
+          </div>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default withAuthAsync(Category, true);
