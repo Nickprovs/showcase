1646592b77bff581820b72c78753f61e9609f2cf
Nick Provost <nickprovs@hotmail.com>
2020-04-20 22:38:37 -0400

    Experimented with adding the necessary styles to embedded video html string posts.

--- client/pages/showcase/video.js
+++ client/pages/showcase/video.js
@@ -2,20 +2,37 @@ import Layout from "../../components/layout";
 import { getVideosAsync } from "../../services/videoService";
 import videoStyles from "../../styles/video.module.css";
 import withAuthAsync from "../../components/common/withAuthAsync";
+import { useEffect, useRef } from "react";
 
 function Video(props) {
   const { videos, user } = props;
+
+  let refs = useRef([]);
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    for (let outerVideoElement of refs.current) outerVideoElement.firstElementChild.className += videoStyles.containerFitImage;
+  });
+
   return (
     <Layout user={user}>
       <div className={videoStyles.container}>
-        {videos.map(video => (
+        {videos.map((video, i) => (
           <div className={videoStyles.item}>
             <div className={videoStyles.title}>
               <h2>{video.title.toUpperCase()}</h2>
             </div>
             <div className={videoStyles.video}>
               {/*TODO: Changed to iFrame or Video Tag*/}
-              <img className={videoStyles.containerFitImage} src={video.src} />
+              <div
+                style={{ width: "100%", height: "100%" }}
+                ref={(ref) => refs.current.push(ref)}
+                dangerouslySetInnerHTML={{
+                  className: videoStyles.containerFitImage,
+                  __html: `
+                  <iframe src="https://player.vimeo.com/video/138343914" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>`,
+                }}
+              ></div>
             </div>
             <div className={videoStyles.text}>
               <label>{video.description}</label>
@@ -27,11 +44,11 @@ function Video(props) {
   );
 }
 
-Video.getInitialProps = async function() {
+Video.getInitialProps = async function () {
   const res = await getVideosAsync();
   console.log("Got Data", res);
   return {
-    videos: res.videos
+    videos: res.videos,
   };
 };
 
