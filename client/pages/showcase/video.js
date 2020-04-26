58befaed51ec7c8803016ef900a2356306e5b9dc
Nick Provost <nickprovs@hotmail.com>
2020-04-26 00:17:05 -0400

    Converted video page to class component and fixed a styling bug.

--- client/pages/showcase/video.js
+++ client/pages/showcase/video.js
@@ -3,53 +3,61 @@ import { getVideosAsync } from "../../services/videoService";
 import videoStyles from "../../styles/video.module.css";
 import withAuthAsync from "../../components/common/withAuthAsync";
 import { useEffect, useRef } from "react";
+import { Component } from "react";
 
-function Video(props) {
-  const { videos, user } = props;
+class Video extends Component {
+  static async getInitialProps(context) {
+    const res = await getVideosAsync();
+    return {
+      videos: res.videos,
+    };
+  }
 
-  let refs = useRef([]);
+  constructor(props) {
+    super(props);
+    this.videoContainerRefs = [];
+  }
 
-  useEffect(() => {
-    // Update the document title using the browser API
-    for (let outerVideoElement of refs.current) outerVideoElement.firstElementChild.className += videoStyles.containerFitImage;
-  });
-
-  return (
-    <Layout user={user}>
-      <div className={videoStyles.container}>
-        {videos.map((video, i) => (
-          <div className={videoStyles.item}>
-            <div className={videoStyles.title}>
-              <h2>{video.title.toUpperCase()}</h2>
-            </div>
-            <div className={videoStyles.video}>
-              {/*TODO: Changed to iFrame or Video Tag*/}
-              <div
-                style={{ width: "100%", height: "100%" }}
-                ref={(ref) => refs.current.push(ref)}
-                dangerouslySetInnerHTML={{
-                  className: videoStyles.containerFitImage,
-                  __html: `
-                  <iframe src="https://player.vimeo.com/video/138343914" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>`,
-                }}
-              ></div>
-            </div>
-            <div className={videoStyles.text}>
-              <label>{video.description}</label>
-            </div>
-          </div>
-        ))}
-      </div>
-    </Layout>
-  );
-}
-
-Video.getInitialProps = async function () {
-  const res = await getVideosAsync();
-  console.log("Got Data", res);
-  return {
-    videos: res.videos,
+  setVideoContainerRefs = (ref) => {
+    this.videoContainerRefs.push(ref);
   };
-};
+
+  componentDidMount() {
+    for (let videoContainer of this.videoContainerRefs) videoContainer.firstElementChild.className += videoStyles.containerFitImage;
+  }
+
+  render() {
+    const { videos, user } = this.props;
+
+    return (
+      <Layout user={user}>
+        <div className={videoStyles.container}>
+          {videos.map((video, i) => (
+            <div key={video._id} className={videoStyles.item}>
+              <div className={videoStyles.title}>
+                <h2>{video.title.toUpperCase()}</h2>
+              </div>
+              <div className={videoStyles.video}>
+                {/*TODO: Changed to iFrame or Video Tag*/}
+                <div
+                  style={{ width: "100%", height: "100%" }}
+                  ref={this.setVideoContainerRefs}
+                  dangerouslySetInnerHTML={{
+                    className: videoStyles.containerFitImage,
+                    __html: `
+                    <iframe src="https://player.vimeo.com/video/138343914" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>`,
+                  }}
+                ></div>
+              </div>
+              <div className={videoStyles.text}>
+                <label>{video.description}</label>
+              </div>
+            </div>
+          ))}
+        </div>
+      </Layout>
+    );
+  }
+}
 
 export default withAuthAsync(Video);
