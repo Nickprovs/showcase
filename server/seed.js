c56daf50e362b828e1baf5dc6a20998b67916da2
Nick Provost <nickprovs@hotmail.com>
2020-04-26 23:15:18 -0400

    removed some unecessary markup from seed

--- server/seed.js
+++ server/seed.js
@@ -358,8 +358,7 @@ const data = {
         {
           title: "Coming Home",
           description: "A film by dooster.",
-          markup: `<iframe src="https://player.vimeo.com/video/406878212?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-          <p><a href="https://vimeo.com/406878212">COMING HOME - Ag Teacht Abhaile</a> from <a href="https://vimeo.com/dooster">Dooster</a> on <a href="https://vimeo.com">Vimeo</a>.</p>`,
+          markup: `<iframe src="https://player.vimeo.com/video/406878212?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>`,
           tags: ["coming", "home", "dooster"],
         },
       ],
