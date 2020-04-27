c88cc9ba80427449a89427d7c7180c55c9fd70d3
Nick Provost <nickprovs@hotmail.com>
2020-04-26 21:51:04 -0400

    made client side video service for api interaction

--- client/services/videoService.js
+++ client/services/videoService.js
@@ -1,138 +1,112 @@
 import asyncUtilities from "../util/asyncUtilities";
+import fetch from "isomorphic-unfetch";
+import { APIURL } from "../util/constants";
+import { Component } from "react";
 
-//Note: Use Isomorphic-Unfetch when actually making rest calls
-//Unlike other http clients, it'll work on server and client
+const PHOTOSAPIURL = `${APIURL}/videos`;
+const PHOTOCATEGORIESURL = `${APIURL}/videoCategories`;
 
-export async function getVideosAsync() {
-  await asyncUtilities.setTimeoutAsync(200);
-  return {
-    videos: [
-      {
-        title: "Epoch 1",
-        description: "This is some preview text. Ya dig?",
-        datePosted: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        dateLastModified: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        orientation: "landscape",
-        size: "large",
-        src: "https://cdna.artstation.com/p/assets/images/images/023/453/030/large/andreas-rocha-wizardstower01.jpg"
-      },
-      {
-        title: "Epoch 2",
-        description: "This is some preview text. Ya dig?",
-        datePosted: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        dateLastModified: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        orientation: "portrait",
-        size: "large",
-        src: "https://cdnb.artstation.com/p/assets/images/images/023/551/031/large/jiwoo-lee-jiwoo-padawan-fanart.jpg"
-      },
-      {
-        title: "Epoch 3",
-        description: "This is some preview text. Ya dig?",
-        datePosted: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        dateLastModified: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        orientation: "portrait",
-        size: "large",
-        src: "https://cdnb.artstation.com/p/assets/images/images/023/723/551/small/ishmael-hoover-eaglewarrior3.jpg"
-      },
-      {
-        title: "Epoch 4",
-        description: "This is some preview text. Ya dig?",
-        datePosted: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        dateLastModified: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        orientation: "landscape",
-        size: "medium",
-        src: "https://cdna.artstation.com/p/assets/images/images/002/912/664/small/andreas-rocha-thegathering.jpg"
-      },
-      {
-        title: "Epoch 5",
-        description: "This is some preview text. Ya dig?",
-        datePosted: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        dateLastModified: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        orientation: "portrait",
-        size: "medium",
-        src: "https://cdnb.artstation.com/p/assets/images/images/000/403/027/small/andreas-rocha-ill-tidings02.jpg"
-      },
-      {
-        title: "Epoch 6",
-        description: "This is some preview text. Ya dig?",
-        datePosted: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        dateLastModified: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        orientation: "landscape",
-        size: "medium",
-        src: "https://cdna.artstation.com/p/assets/images/images/000/085/508/small/Alm-Atias-Refuge.jpg"
-      },
-      {
-        title: "Epoch 7",
-        description: "This is some preview text. Ya dig?",
-        datePosted: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        dateLastModified: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        orientation: "landscape",
-        size: "medium",
-        src: "https://cdnb.artstation.com/p/assets/images/images/000/320/963/small/andreas-rocha-dangerousroads.jpg"
-      },
-      {
-        title: "Epoch 8",
-        description: "This is some preview text. Ya dig?",
-        datePosted: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        dateLastModified: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        orientation: "portrait",
-        size: "medium",
-        src: "https://cdnb.artstation.com/p/assets/images/images/001/058/155/large/mike-hill-002-front-view3-copy.jpg"
-      },
-      {
-        title: "Epoch 9",
-        description: "This is some preview text. Ya dig?",
-        datePosted: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        dateLastModified: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        orientation: "portrait",
-        size: "medium",
-        src: "https://cdnb.artstation.com/p/assets/images/images/000/286/213/small/sparth-firststrike-final.jpg"
-      },
-      {
-        title: "Epoch 10",
-        description: "This is some preview text. Ya dig?",
-        datePosted: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        dateLastModified: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        orientation: "landscape",
-        size: "medium",
-        src: "https://cdnb.artstation.com/p/assets/images/images/000/244/079/large/aaron-limonick-lake-vista-key-2-logo.jpg"
-      },
-      {
-        title: "Epoch 11",
-        description: "This is some preview text. Ya dig?",
-        datePosted: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        dateLastModified: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        orientation: "portrait",
-        size: "medium",
-        src: "https://cdnb.artstation.com/p/assets/images/images/000/201/563/small/yuri-shwedoff-ellie-the-last-of-us.jpg"
-      },
-      {
-        title: "Epoch 12",
-        description: "This is some preview text. Ya dig?",
-        datePosted: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        dateLastModified: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        orientation: "portrait",
-        size: "medium",
-        src: "https://cdna.artstation.com/p/assets/images/images/018/461/496/small/moero-knight-0005-magical-girls.jpg"
-      },
-      {
-        title: "Epoch 13",
-        description: "This is some preview text. Ya dig?",
-        datePosted: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        dateLastModified: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        orientation: "landscape",
-        size: "medium",
-        src: "https://cdna.artstation.com/p/assets/images/images/019/964/822/large/kyle-grech-spike.jpg"
-      },
-      {
-        title: "Epoch 14",
-        description: "This is some preview text. Ya dig?",
-        datePosted: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        dateLastModified: "Thu Dec 29 2011 20:14:56 GMT-0600 (CST)",
-        orientation: "landscape",
-        size: "large",
-        src: "https://cdna.artstation.com/p/assets/images/images/017/735/130/large/ege-ozteke-champaloo.jpg"
-      }
-    ]
-  };
+export async function getVideosAsync(options) {
+  let query = "";
+  if (options.limit) query += query ? `&limit=${options.limit}` : `?limit=${options.limit}`;
+  if (options.offset) query += query ? `&offset=${options.offset}` : `?offset=${options.offset}`;
+  if (options.search) query += query ? `&search=${options.search}` : `?search=${options.search}`;
+  if (options.category) query += query ? `&category=${options.category}` : `?category=${options.category}`;
+
+  const res = await fetch(PHOTOSAPIURL + query);
+  return res;
+}
+
+export async function getVideoAsync(videoId) {
+  const requestUrl = PHOTOSAPIURL + "/" + videoId;
+  const res = await fetch(requestUrl);
+  return res;
+}
+
+export async function createVideoAsync(video) {
+  const res = await fetch(PHOTOSAPIURL, {
+    method: "post",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(video),
+  });
+  return res;
+}
+
+export async function updateVideoAsync(video) {
+  const body = { ...video };
+  delete body._id;
+
+  const res = await fetch(`${PHOTOSAPIURL}/${video._id}`, {
+    method: "put",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  return res;
+}
+
+export async function deleteVideoAsync(videoId) {
+  const res = await fetch(`${PHOTOSAPIURL}/${videoId}`, {
+    method: "delete",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return res;
+}
+
+export async function getVideoCategoryAsync(categoryId) {
+  const requestUrl = PHOTOCATEGORIESURL + "/" + categoryId;
+  const res = await fetch(requestUrl);
+  return res;
+}
+
+export async function getVideoCategoriesAsync() {
+  const res = await fetch(PHOTOCATEGORIESURL);
+  return res;
+}
+
+export async function createVideoCategoryAsync(name, slug) {
+  const res = await fetch(PHOTOCATEGORIESURL, {
+    method: "post",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name: name, slug: slug }),
+  });
+  return res;
+}
+
+export async function updateVideoCategoryAsync(category) {
+  const body = { ...category };
+  delete body._id;
+
+  const res = await fetch(`${PHOTOCATEGORIESURL}/${category._id}`, {
+    method: "put",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  return res;
+}
+
+export async function deleteVideoCategoryAsync(videoCategoryId) {
+  console.log("Making request to", `${PHOTOCATEGORIESURL}/${videoCategoryId}`);
+  const res = await fetch(`${PHOTOCATEGORIESURL}/${videoCategoryId}`, {
+    method: "delete",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return res;
 }
