f4fecc43fd1d49a3b5b42b755ec638c7b2e3db71
Nick Provost <nickprovs@hotmail.com>
2020-05-05 22:40:32 -0400

    formatting

--- client/pages/blog/[slug].js
+++ client/pages/blog/[slug].js
@@ -2,12 +2,12 @@ import Layout from "../../components/layout";
 import { getBlogAsync } from "../../services/blogService";
 import withAuthAsync from "../../components/common/withAuthAsync";
 
-Blog.getInitialProps = async function(context) {
+Blog.getInitialProps = async function (context) {
   const { slug } = context.query;
   const res = await getBlogAsync(slug);
   const blog = await res.json();
   return {
-    blog: blog
+    blog: blog,
   };
 };
 
@@ -16,7 +16,7 @@ function Blog({ blog, user }) {
   return (
     <Layout user={user}>
       <h1>{blog.title}</h1>
-      <div dangerouslySetInnerHTML={{__html: blog.body}}/>
+      <div dangerouslySetInnerHTML={{ __html: blog.body }} />
     </Layout>
   );
 }
