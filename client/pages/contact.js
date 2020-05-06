8be4d0e330d759ff4b3ffa3a8eff7a5ff8a38ae8
Nick Provost <nickprovs@hotmail.com>
2020-05-05 22:25:17 -0400

    Improved the front-end contact form experience.

--- client/pages/contact.js
+++ client/pages/contact.js
@@ -5,6 +5,7 @@ import Form from "../components/common/form";
 import { contactAsync } from "../services/contactService";
 import { toast } from "react-toastify";
 import getConfig from "next/config";
+import NProgress from "nprogress";
 
 const { publicRuntimeConfig } = getConfig();
 const recaptchaSiteKey = publicRuntimeConfig.recaptchaSiteKey;
@@ -13,13 +14,16 @@ class Contact extends Form {
   constructor() {
     super();
 
-    this.state.data = { name: "", email: "", message: "", captcha: "" };
+    this.originalFormData = { name: "", email: "", message: "", captcha: "" };
+
+    this.state.contactComplete = false;
+    this.state.data = this.originalFormData;
     this.state.errors = {};
   }
 
   schema = Joi.object({
-    name: Joi.string().min(5).max(30).required().label("Name"),
-    email: Joi.string().min(6).max(100).required().label("Email"),
+    name: Joi.string().min(3).max(30).required().label("Name"),
+    email: Joi.string().min(5).max(100).required().label("Email"),
     message: Joi.string().min(6).max(1000).required().label("Message"),
     captcha: Joi.string().required().label("Captcha"),
   });
@@ -30,13 +34,17 @@ class Contact extends Form {
     let res = null;
     //Try and post the new category
     try {
+      NProgress.start();
       res = await contactAsync(contactData);
     } catch (ex) {
       let errorMessage = `Error: ${ex}`;
       console.log(errorMessage);
       toast.error(errorMessage);
       return;
+    } finally {
+      NProgress.done();
     }
+
     if (!res.ok) {
       let body = "";
       //TODO: Parse Text OR JSON
@@ -47,23 +55,44 @@ class Contact extends Form {
       return;
     }
 
-    // //TODO: Disallow posting duplicate category at server level.
-    // Router.push("/blog");
+    //Set the contact flow as complete and wipe the data from the form
+    this.setState({ contactComplete: true });
+
+    //Wipe the form state in case we want to present the form again at some future point.
+    this.setState({ data: this.originalFormData });
+    this.resetRecaptcha();
   };
+
+  getContactCompleteMarkup() {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <h1>{`Thanks for reaching out! I'll get back to you as soon as possible!`}</h1>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <object style={{ display: "block", width: "35%", overflow: "none" }} type="image/svg+xml" data="/director_happy.svg"></object>
+        </div>
+      </div>
+    );
+  }
+
+  getContactNotCompleteMarkup() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        {this.renderTextInput("name", "NAME")}
+        {this.renderTextInput("email", "EMAIL")}
+        {this.renderTextArea("message", "MESSAGE")}
+        {this.renderRecaptcha("captcha", "CAPTCHA", recaptchaSiteKey)}
+        {this.renderButton("SEND MESSAGE")}
+      </form>
+    );
+  }
 
   render() {
     const { user } = this.props;
+    const { contactComplete } = this.state;
+
     return (
       <Layout user={user}>
-        <div className="standardPadding">
-          <form onSubmit={this.handleSubmit}>
-            {this.renderTextInput("name", "NAME")}
-            {this.renderTextInput("email", "EMAIL")}
-            {this.renderTextArea("message", "MESSAGE")}
-            {this.renderRecaptcha("captcha", "CAPTCHA", recaptchaSiteKey)}
-            {this.renderButton("SEND MESSAGE")}
-          </form>
-        </div>
+        <div className="standardPadding">{!contactComplete ? this.getContactNotCompleteMarkup() : this.getContactCompleteMarkup()}</div>
       </Layout>
     );
   }
--- server/routes/schemas/body/contact/contact.js
+++ server/routes/schemas/body/contact/contact.js
@@ -2,7 +2,7 @@ const Joi = require("@hapi/joi");
 Joi.objectId = require("joi-objectid")(Joi);
 
 module.exports = schema = Joi.object({
-  name: Joi.string().min(0).max(80),
+  name: Joi.string().min(3).max(80),
   email: Joi.string().email(),
   message: Joi.string().min(0).max(1000),
   captcha: Joi.string(),
