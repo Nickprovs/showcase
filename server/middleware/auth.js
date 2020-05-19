5162ac0233ae96605cc83efb638cbfda71678fc6
Nick Provost <nickprovs@hotmail.com>
2020-05-19 15:55:07 -0400

    removed "requiresAuth" as a thing

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,8 +3,6 @@ const config = require("config");
 const winston = require("winston");
 
 module.exports = function (req, res, next) {
-  if (!config.get("requiresAuth")) return next();
-
   const token = req.header("x-auth-token") ? req.header("x-auth-token") : req.cookies.showcase_accessToken;
   if (!token) return res.status(401).send("Access denied. No token provided.");
 
