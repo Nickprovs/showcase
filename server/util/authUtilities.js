43eae894bba75f3b985c70eae3243176cf58d0ad
Nick Provost <nickprovs@hotmail.com>
2020-05-20 23:59:19 -0400

    Beefed up email mfa code security.

--- server/util/authUtilities.js
+++ server/util/authUtilities.js
@@ -3,8 +3,8 @@ const moment = require("moment");
 
 module.exports = class AuthUtilities {
   static getMfaCodeObject() {
-    const mfaCode = StringUtilities.generateRandomUppercaseAlphaNumericString(6);
-    const mfaCodeExpiresAt = moment().add("10", "minutes").toJSON();
+    const mfaCode = StringUtilities.generateRandomUppercaseAlphaNumericString(8);
+    const mfaCodeExpiresAt = moment().add("5", "minutes").toJSON();
     return {
       code: mfaCode,
       expiresAt: mfaCodeExpiresAt,
--- server/util/stringUtilities.js
+++ server/util/stringUtilities.js
@@ -2,10 +2,7 @@ module.exports = class StringUtilities {
   static generateRandomUppercaseAlphaNumericString(length) {
     let text = "";
     let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-    for (let i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
+    for (let i = 0; i < length; i++) text += possible.charAt(Math.floor(Math.random() * possible.length));
     return text;
   }
 };
