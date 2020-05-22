01d46d3741f622d9d10e82398f8041e713f72b40
Nick Provost <nickprovs@hotmail.com>
2020-05-21 23:24:04 -0400

    fixed a security issue in me route

--- server/routes/me.js
+++ server/routes/me.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 
 router.get("/", auth(), async (req, res) => {
-  let user = await User.findOne({ username: req.user.username }).select("-password");
+  let user = await User.findOne({ username: req.user.username }).select("-password -mfaCode");
   res.send(user);
 });
 
