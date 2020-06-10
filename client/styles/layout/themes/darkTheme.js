bcfa622f05c77d9073db56bf56c6715f670de945
Nick Provost <nickprovs@hotmail.com>
2020-06-09 22:46:07 -0400

    added dark theme

--- client/styles/layout/themes/darkTheme.js
+++ client/styles/layout/themes/darkTheme.js
@@ -1,9 +1,20 @@
 const DarkTheme = {
-  "--b1": "#000000",
-  "--b2": "#1a1a1a",
-  "--b3": "#2b2b2b",
+  "--b1": "#1a1a1b",
+  "--b2": "#2c2c2e",
+  "--b3": "#dedede",
   "--f1": "#FFFFFF",
-  "--s1": "#33C9FF"
+  "--f2": "#FFFFFF",
+  "--s1": "#FFFFFF",
+  "--s2": "rgba(255,255,255,0.5)",
+  "--s3": "rgba(255,255,255,0.4)",
+  "--s4": "rgba(0,0,0,0.7)",
+  "--s5": "#18bfef",
+  "--s6": "#1f9ec3",
+  "--s7": "#FFFFFF",
+  "--s8": "#3d3d3d",
+  "--s9": "#green",
+  "--s10": "red",
+  "--s11": "#dbdbdb",
 };
 
 export default DarkTheme;
