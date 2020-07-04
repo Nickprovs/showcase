e5df8503de4b187ff7fe664952c92a78c4244b3d
Nick Provost <nickprovs@hotmail.com>
2020-07-04 13:14:54 -0400

    fixed blog integration tests

--- server/tests/integration/blog.test.js
+++ server/tests/integration/blog.test.js
@@ -1,19 +1,19 @@
 const request = require("supertest");
-const { Article } = require("../../models/article");
-const { ArticleCategory } = require("../../models/articleCategory");
+const { Blog } = require("../../models/blog");
+const { BlogCategory } = require("../../models/blogCategory");
 const { User } = require("../../models/user");
 const mongoose = require("mongoose");
 const moment = require("moment");
 
 let server;
-describe("/articles", () => {
+describe("/blogs", () => {
   beforeEach(() => {
     server = require("../../index");
   });
   afterEach(async () => {
     await server.close();
-    await Article.deleteMany({});
-    await ArticleCategory.deleteMany({});
+    await Blog.deleteMany({});
+    await BlogCategory.deleteMany({});
   });
 
   describe("GET /", () => {
@@ -24,61 +24,61 @@ describe("/articles", () => {
     let article3;
 
     beforeEach(async () => {
-      await Article.deleteMany({});
-      await ArticleCategory.deleteMany({});
+      await Blog.deleteMany({});
+      await BlogCategory.deleteMany({});
 
-      articleCategory1 = new ArticleCategory({ name: "Fiction", slug: "fiction" });
+      articleCategory1 = new BlogCategory({ name: "Fiction", slug: "fiction" });
       articleCategory1 = await articleCategory1.save();
 
-      articleCategory2 = new ArticleCategory({ name: "Non-Fiction", slug: "non-fiction" });
+      articleCategory2 = new BlogCategory({ name: "Non-Fiction", slug: "non-fiction" });
       articleCategory2 = await articleCategory2.save();
 
-      article1 = new Article({
+      article1 = new Blog({
         slug: "the-great-cow-jumped-over-the-moon",
         title: "The great cow jumped over the moon",
         category: articleCategory1,
         description: "The cowiest of cows.",
         image: "https://i.imgur.com/O2NQNvP.jpg",
         body: "aadada",
-        tags: ["the", "great", "cow", "common1", "common2"]
+        tags: ["the", "great", "cow", "common1", "common2"],
       });
       await article1.save();
 
-      article2 = new Article({
+      article2 = new Blog({
         slug: "the-dog-jumped-over-the-fence",
         title: "The dog jumped over the fence",
         category: articleCategory2,
         description: "The dogiest of dogs.",
         image: "https://i.imgur.com/O2NQNvP.jpg",
         body: "aadasfsfsfsfsfsfsfsda",
-        tags: ["the", "great", "cow", "common1", "common2"]
+        tags: ["the", "great", "cow", "common1", "common2"],
       });
       await article2.save();
 
-      article3 = new Article({
+      article3 = new Blog({
         slug: "the-beaver-jumped-over-the-fence",
         title: "The beaver jumped over the fence",
         category: articleCategory2,
         description: "The beaveriest of beavers.",
         image: "https://i.imgur.com/O2NQNvP.jpg",
         body: "aadasfsfsfsfsfsfsfsda",
-        tags: ["the", "beaver", "beav"]
+        tags: ["the", "beaver", "beav"],
       });
       await article3.save();
     });
 
     it("Should return all the articles when no query filter is provided", async () => {
-      const res = await request(server).get("/articles");
+      const res = await request(server).get("/blogs");
 
       expect(res.status).toBe(200);
       expect(res.body.items.length).toBe(3);
-      expect(res.body.items.some(g => g.title === article1.title)).toBeTruthy();
-      expect(res.body.items.some(g => g.title === article2.title)).toBeTruthy();
-      expect(res.body.items.some(g => g.title === article3.title)).toBeTruthy();
+      expect(res.body.items.some((g) => g.title === article1.title)).toBeTruthy();
+      expect(res.body.items.some((g) => g.title === article2.title)).toBeTruthy();
+      expect(res.body.items.some((g) => g.title === article3.title)).toBeTruthy();
     });
 
     it("Should return the correct metadata when no query filter is provided", async () => {
-      const res = await request(server).get("/articles");
+      const res = await request(server).get("/blogs");
 
       expect(res.body.total === 3);
       expect(res.body.offset === 0);
@@ -86,16 +86,16 @@ describe("/articles", () => {
     });
 
     it("Should return only the articles that match the category id filter", async () => {
-      const res = await request(server).get(`/articles?category=${articleCategory2._id}`);
+      const res = await request(server).get(`/blogs?category=${articleCategory2._id}`);
 
       expect(res.status).toBe(200);
       expect(res.body.items.length).toBe(2);
-      expect(res.body.items.some(g => g.title === article2.title)).toBeTruthy();
-      expect(res.body.items.some(g => g.title === article3.title)).toBeTruthy();
+      expect(res.body.items.some((g) => g.title === article2.title)).toBeTruthy();
+      expect(res.body.items.some((g) => g.title === article3.title)).toBeTruthy();
     });
 
     it("Should return the correct metadata that matches the category id filter", async () => {
-      const res = await request(server).get(`/articles?category=${articleCategory2._id}`);
+      const res = await request(server).get(`/blogs?category=${articleCategory2._id}`);
 
       expect(res.body.items.length).toBe(2);
       expect(res.body.total === 2);
@@ -104,7 +104,7 @@ describe("/articles", () => {
     });
 
     it("Should return the correct metadata when providing an offset", async () => {
-      const res = await request(server).get(`/articles?offset=1`);
+      const res = await request(server).get(`/blogs?offset=1`);
 
       expect(res.body.items.length).toBe(2);
       expect(res.body.total === 3);
@@ -113,7 +113,7 @@ describe("/articles", () => {
     });
 
     it("Should return the correct metadata that matches offset and the category id filter", async () => {
-      const res = await request(server).get(`/articles?category=${articleCategory2._id}&offset=1`);
+      const res = await request(server).get(`/blogs?category=${articleCategory2._id}&offset=1`);
 
       expect(res.body.items.length).toBe(1);
       expect(res.body.total === 2);
@@ -122,7 +122,7 @@ describe("/articles", () => {
     });
 
     it("Should return the multiple of articles that match the tag search", async () => {
-      const res = await request(server).get(`/articles?search=common1`);
+      const res = await request(server).get(`/blogs?search=common1`);
 
       //The items returned should nor have any results whose tags don't include "common1" or "common2"
       expect(res.body.items.length).toBe(2);
@@ -130,60 +130,56 @@ describe("/articles", () => {
     });
 
     it("Should return the singular article that matches the tag search", async () => {
-      const res = await request(server).get(`/articles?search=beaver`);
+      const res = await request(server).get(`/blogs?search=beaver`);
 
       expect(res.body.items.length).toBe(1);
       expect(res.body.total === 1);
     });
 
     it("Should return the singular article that matches the title search", async () => {
-      const res = await request(server).get(`/articles?search=dog`);
+      const res = await request(server).get(`/blogs?search=dog`);
 
       expect(res.body.items.length).toBe(1);
       expect(res.body.total === 1);
     });
 
     it("Should return the multiple articles that matches the title search", async () => {
-      const res = await request(server).get(`/articles?search=jumped`);
+      const res = await request(server).get(`/blogs?search=jumped`);
 
       expect(res.body.items.length).toBe(3);
       expect(res.body.total === 3);
     });
 
     it("Should return the singular article that matches the description search", async () => {
-      const res = await request(server).get(`/articles?search=cowiest`);
+      const res = await request(server).get(`/blogs?search=cowiest`);
 
       expect(res.body.items.length).toBe(1);
       expect(res.body.total === 1);
-    });
-
-    it("Should return the multiple articles that matches the category search", async () => {
-      const res = await request(server).get(`/articles?search=Non-Fiction`);
-      expect(res.body.items.length).toBe(2);
-      expect(res.body.total === 2);
     });
   });
 
   describe("GET /:id", () => {
     it("should return a article if valid id is passed", async () => {
-      let articleCategory = new ArticleCategory({ name: "Fiction", slug: "fiction" });
+      let articleCategory = new BlogCategory({ name: "Fiction", slug: "fiction" });
       articleCategory = await articleCategory.save();
 
-      const article = new Article({
+      const article = new Blog({
         slug: "the-great-tiger-jumped-over-the-moon",
         title: "The great tiger jumped over the moon",
         category: articleCategory,
         description: "The dogiest of dogs.",
         image: "https://i.imgur.com/O2NQNvP.jpg",
         body: "aadada",
-        addressableHighlight: {
-          label: "Cool Search Engine",
-          address: "www.google.com"
-        },
-        tags: ["the", "great", "tiger"]
+        addressableHighlights: [
+          {
+            label: "Cool Search Engine",
+            address: "www.google.com",
+          },
+        ],
+        tags: ["the", "great", "tiger"],
       });
       await article.save();
-      const res = await request(server).get("/articles/" + article._id);
+      const res = await request(server).get("/blogs/" + article._id);
 
       expect(res.status).toBe(200);
       expect(res.body).toHaveProperty("slug", article.slug);
@@ -195,19 +191,19 @@ describe("/articles", () => {
       expect(res.body).toHaveProperty("description", article.description);
       expect(res.body).toHaveProperty("image", article.image);
       expect(res.body).toHaveProperty("body", article.body);
-      expect(res.body).toHaveProperty("addressableHighlight");
+      expect(res.body).toHaveProperty("addressableHighlights");
       expect(res.body).toHaveProperty("tags");
       expect(res.body).toHaveProperty("contingency");
     });
 
     it("should return 400 if invalid id is passed", async () => {
-      const res = await request(server).get("/articles/-1");
+      const res = await request(server).get("/blogs/-1");
       expect(res.status).toBe(400);
     });
 
     it("should return 404 if no article with the given id exists", async () => {
       const id = mongoose.Types.ObjectId();
-      const res = await request(server).get("/articles/" + id);
+      const res = await request(server).get("/blogs/" + id);
 
       expect(res.status).toBe(404);
     });
@@ -218,16 +214,13 @@ describe("/articles", () => {
     let token;
 
     const exec = () => {
-      return request(server)
-        .post("/articles")
-        .set("x-auth-token", token)
-        .send(article);
+      return request(server).post("/blogs").set("x-auth-token", token).send(article);
     };
 
     beforeEach(async () => {
       token = new User({ username: "adminUser", isAdmin: true }).generateAuthToken();
 
-      let articleCategory = new ArticleCategory({ name: "Fiction", slug: "fiction" });
+      let articleCategory = new BlogCategory({ name: "Fiction", slug: "fiction" });
       articleCategory = await articleCategory.save();
 
       article = {
@@ -237,16 +230,18 @@ describe("/articles", () => {
         description: "The dogiest of dogs.",
         image: "https://i.imgur.com/O2NQNvP.jpg",
         body: "aadada",
-        addressableHighlight: {
-          label: "Cool Search Engine",
-          address: "www.google.com"
-        },
+        addressableHighlights: [
+          {
+            label: "Cool Search Engine",
+            address: "www.google.com",
+          },
+        ],
         tags: ["The", "Dogiest", "Dog"],
         contingency: {
           key1: "hey",
           key2: "world",
-          key3: "what's up"
-        }
+          key3: "what's up",
+        },
       };
     });
 
@@ -285,7 +280,7 @@ describe("/articles", () => {
     it("should save the article if it is valid", async () => {
       await exec();
 
-      const article = await Article.find({ title: "The dogiest of dogs" });
+      const article = await Blog.find({ title: "The dogiest of dogs" });
 
       expect(article).not.toBeNull();
     });
@@ -303,7 +298,7 @@ describe("/articles", () => {
       expect(res.body).toHaveProperty("body", article.body);
       expect(res.body).toHaveProperty("tags");
       expect(res.body).toHaveProperty("contingency", article.contingency);
-      expect(res.body).toHaveProperty("addressableHighlight");
+      expect(res.body).toHaveProperty("addressableHighlights");
     });
   });
 
@@ -316,16 +311,16 @@ describe("/articles", () => {
 
     const exec = () => {
       return request(server)
-        .put("/articles/" + id)
+        .put("/blogs/" + id)
         .set("x-auth-token", token)
         .send(article);
     };
 
     beforeEach(async () => {
-      articleCategory = new ArticleCategory({ name: "Fiction", slug: "fiction" });
+      articleCategory = new BlogCategory({ name: "Fiction", slug: "fiction" });
       articleCategory = await articleCategory.save();
 
-      existingArticle = new Article({
+      existingArticle = new Blog({
         slug: "the-original-dogiest-of-dogs",
         title: "The original dogiest of dogs",
         category: articleCategory,
@@ -333,15 +328,17 @@ describe("/articles", () => {
         image: "https://i.imgur.com/O2NQNvP.jpg",
         body: "aaadeeadada",
         tags: ["The", "Dogiest", "Dog"],
-        addressableHighlight: {
-          label: "Cool Search Engine",
-          address: "www.google.com"
-        },
+        addressableHighlights: [
+          {
+            label: "Cool Search Engine",
+            address: "www.google.com",
+          },
+        ],
         contingency: {
           key1: "Hi",
           key2: "What's good?",
-          key3: "This is wack!"
-        }
+          key3: "This is wack!",
+        },
       });
       await existingArticle.save();
 
@@ -355,16 +352,18 @@ describe("/articles", () => {
         image: "https://i.imgur.com/O2NQNvP.jpg",
         body: "aaaabbbbbccccddddeeeffffadada",
         tags: ["The", "Dogiest", "Dog"],
-        addressableHighlight: {
-          label: "Bad Search Engine",
-          address: "www.bing.com"
-        },
+        addressableHighlights: [
+          {
+            label: "Bad Search Engine",
+            address: "www.bing.com",
+          },
+        ],
         contingency: {
           key1: "THIS IS EDITED",
           key2: "WOWWWW",
           key3: "CRAZY",
-          key4: "A new key was added too!!!"
-        }
+          key4: "A new key was added too!!!",
+        },
       };
     });
 
@@ -416,7 +415,7 @@ describe("/articles", () => {
     it("should save the article if it is valid", async () => {
       await exec();
 
-      const article = await Article.find({ title: "testtt1" });
+      const article = await Blog.find({ title: "testtt1" });
 
       expect(article).not.toBeNull();
     });
@@ -424,7 +423,7 @@ describe("/articles", () => {
     it("should update the article if input is valid", async () => {
       await exec();
 
-      const updatedArticle = await Article.findById(existingArticle._id);
+      const updatedArticle = await Blog.findById(existingArticle._id);
 
       expect(updatedArticle.title).toBe(article.title);
       expect(updatedArticle.contingency.get("key4")).toBeTruthy();
@@ -447,7 +446,7 @@ describe("/articles", () => {
       expect(res.body).toHaveProperty("body", article.body);
       expect(res.body).toHaveProperty("tags", article.tags);
       expect(res.body).toHaveProperty("contingency", article.contingency);
-      expect(res.body).toHaveProperty("addressableHighlight", article.addressableHighlight);
+      expect(res.body).toHaveProperty("addressableHighlights", article.addressableHighlights);
     });
   });
 
@@ -458,16 +457,16 @@ describe("/articles", () => {
 
     const exec = async () => {
       return await request(server)
-        .delete("/articles/" + id)
+        .delete("/blogs/" + id)
         .set("x-auth-token", token)
         .send();
     };
 
     beforeEach(async () => {
-      articleCategory = new ArticleCategory({ name: "Fiction", slug: "fiction" });
+      articleCategory = new BlogCategory({ name: "Fiction", slug: "fiction" });
       articleCategory = await articleCategory.save();
 
-      article = new Article({
+      article = new Blog({
         slug: "the-original-dogiest-of-dogs",
         title: "The original dogiest of dogs",
         category: articleCategory,
@@ -475,16 +474,18 @@ describe("/articles", () => {
         image: "https://i.imgur.com/O2NQNvP.jpg",
         body: "aaadeeadada",
         tags: ["The", "Dogiest", "Dog"],
-        addressableHighlight: {
-          label: "Bad Search Engine",
-          address: "www.bing.com"
-        },
+        addressableHighlights: [
+          {
+            label: "Bad Search Engine",
+            address: "www.bing.com",
+          },
+        ],
         contingency: {
           key1: "THIS IS EDITED",
           key2: "WOWWWW",
           key3: "CRAZY",
-          key4: "A new key was added too!!!"
-        }
+          key4: "A new key was added too!!!",
+        },
       });
       await article.save();
       id = article._id;
@@ -526,7 +527,7 @@ describe("/articles", () => {
     it("should delete the article if input is valid", async () => {
       await exec();
 
-      const articleInDb = await Article.findById(id);
+      const articleInDb = await Blog.findById(id);
 
       expect(articleInDb).toBeNull();
     });
@@ -547,7 +548,7 @@ describe("/articles", () => {
       expect(res.body).toHaveProperty("body", article.body);
       expect(res.body).toHaveProperty("tags");
       expect(res.body).toHaveProperty("contingency");
-      expect(res.body).toHaveProperty("addressableHighlight");
+      expect(res.body).toHaveProperty("addressableHighlights");
     });
   });
 });
